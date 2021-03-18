9cac2c6c91c983c6c67eb7e7f02223005be3cd84
NN <nhatnghe@gmail.com>
2021-03-18 20:55:52 +0700

    Day 01 - Web  API

---
+++ Buoi01_WebAPI/Buoi01_WebAPI/Models/APICoreReactContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Buoi01_WebAPI.Models
+{
+    public partial class APICoreReactContext : DbContext
+    {
+        public APICoreReactContext()
+        {
+        }
+
+        public APICoreReactContext(DbContextOptions<APICoreReactContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblCities> TblCities { get; set; }
+        public virtual DbSet<TblEmployee> TblEmployee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=APICoreReact;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TblCities>(entity =>
+            {
+                entity.HasKey(e => e.CityId)
+                    .HasName("PK__tblCitie__F2D21A9678BAC296");
+
+                entity.ToTable("tblCities");
+
+                entity.Property(e => e.CityId).HasColumnName("CityID");
+
+                entity.Property(e => e.CityName)
+                    .IsRequired()
+                    .HasMaxLength(20);
+            });
+
+            modelBuilder.Entity<TblEmployee>(entity =>
+            {
+                entity.HasKey(e => e.EmployeeId)
+                    .HasName("PK__tblEmplo__7AD04FF1E581A580");
+
+                entity.ToTable("tblEmployee");
+
+                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.Department)
+                    .IsRequired()
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.EmployeeName)
+                    .IsRequired()
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.Gender)
+                    .IsRequired()
+                    .HasMaxLength(6);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Buoi01_WebAPI/Buoi01_WebAPI/Models/TblCities.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Buoi01_WebAPI.Models
+{
+    public partial class TblCities
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+    }
+}
