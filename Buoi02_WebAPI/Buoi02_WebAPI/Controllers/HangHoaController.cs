1dba0ee5a75a8d08a861de725d9d6dab805811ad
Nhat Nghe <aspcorenn@gmail.com>
2021-03-23 20:20:09 +0700

    Add API get list

--- Buoi02_WebAPI/Buoi02_WebAPI/Controllers/HangHoaController.cs
+++ Buoi02_WebAPI/Buoi02_WebAPI/Controllers/HangHoaController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Buoi02_WebAPI.Models;
+using Buoi02_WebAPI.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace Buoi02_WebAPI.Controllers
 {
@@ -11,5 +14,38 @@ namespace Buoi02_WebAPI.Controllers
     [ApiController]
     public class HangHoaController : ControllerBase
     {
+        private readonly NhatNgheWebAPIContext _context;
+        private readonly int SO_PHAN_TU_MOI_TRANG;
+
+        public HangHoaController(NhatNgheWebAPIContext ctx, IConfiguration config)
+        {
+            _context = ctx;
+            SO_PHAN_TU_MOI_TRANG = int.Parse(config["PagingConfig:NumberOfRecord"]);
+        }
+
+        [HttpGet]
+        // host/api/HangHoa?search=xyz&page=1
+        public IActionResult GetAll(string search, int page = 1)
+        {
+            //HttpContext.Request
+            var hangHoa = _context.HangHoa.AsQueryable();
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                hangHoa = hangHoa.Where(hh => hh.TenHh.Contains(search));
+            }
+
+            var data = hangHoa.Skip((page - 1) * SO_PHAN_TU_MOI_TRANG)
+                .Take(SO_PHAN_TU_MOI_TRANG)
+                .Select(hh => new HangHoaVM
+                {
+                    MaHh = hh.MaHh,
+                    TenHh = hh.TenHh,
+                    GiaBan = hh.DonGia,
+                    Hinh = MyTools.GetRealUrl("HangHoa", hh.Hinh, HttpContext.Request)
+                }).ToList();
+            return Ok(data);
+        }
+
     }
 }
--- Buoi02_WebAPI/Buoi02_WebAPI/Startup.cs
+++ Buoi02_WebAPI/Buoi02_WebAPI/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -44,6 +44,9 @@ namespace Buoi02_WebAPI
             }
 
             app.UseHttpsRedirection();
+
+            //Cho phép truy xuất trực tiếp thư mục tĩnh
+            app.UseStaticFiles();
 
             app.UseRouting();
 
