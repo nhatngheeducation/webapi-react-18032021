f6ed7cd4b7b82f362b5cd80c06d3f15421b85c38
Nhat Nghe <aspcorenn@gmail.com>
2021-03-25 18:44:51 +0700

    Add DonHang model

---
+++ Buoi02_WebAPI/Buoi02_WebAPI/ViewModels/DonHang.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buoi02_WebAPI.ViewModels
+{
+    public class DonHang
+    {
+        public string MaKh { get; set; }
+        public string NguoiNhan { get; set; }
+        public string DiaChiGiao { get; set; }
+        public List<DonHangChiTiet> HangHoa { get; set; }
+        public DonHang()
+        {
+            HangHoa = new List<DonHangChiTiet>();
+        }
+    }
+
+    public class DonHangChiTiet
+    {
+        public int MaHH { get; set; }
+        public int SoLuong { get; set; }
+    }
+}
