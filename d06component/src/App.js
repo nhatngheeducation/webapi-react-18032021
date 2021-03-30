9ba7e12cde40785d462273fa16c81d66bfccc306
Nhat Nghe <aspcorenn@gmail.com>
2021-03-30 20:56:10 +0700

    Done Todo

--- d06component/src/App.js
+++ d06component/src/App.js
@@ -11,28 +11,42 @@ function App() {
 
     //Khai báo state chứa danh sách các việc cần làm
     const [items, setItems] = useState([
-        "Đi học ReactJS", "Xem phim", "Nghe nhạc"
+        { "text": "Đi học ReactJS", "done": false },
+        { "text": "Xem phim", "done": true },
+        { "text": "Nghe nhạc", "done": false }
     ]);
     const [value, setValue] = useState("");
     const handleAddItem = () => {
         //lấy giá trị của value thêm bào mảng Items
-        //let newValue = [...items, value]
-        let newValue = [value, ...items]
+        let newValue = [{ "text": value, "done": false }, ...items]
         setItems(newValue);
     }
-
+    const handleComplete = (index) => {
+        console.log(index, items)
+        let newValue = items;
+        newValue[index].done = true;
+        setItems(newValue);
+        console.log("After", items)
+    }
     return (
         <div className="App">
             <h2>Danh sách công việc</h2>
-            {items.map(item => (
-                <div className="todo-item">{item}</div>
+            {items.map((item, idx) => (
+                <div className="todo-item" key={idx} >
+                    {item.text}
+                    {item.done == false ? (
+                        <button className="btn-complete"
+                            onClick={() => handleComplete(idx)}>
+                            Hoàn thành</button>
+                    ) : ''}
+                </div>
             ))}
-            {items.map(item => {
-                let item_kq = item.toUpperCase();
+            {/* {items.map(item => {
+                let item_kq = item.text.toUpperCase();
                 return (
                     <li>{item_kq}</li>
                 );
-            })}
+            })} */}
             <div>
                 Nhập công việc:
                 <input value={value} onChange={e => setValue(e.target.value)} />
