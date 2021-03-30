bf74e1af95aac5673f64082e2a9ca2884c4fcbb6
Nhat Nghe <aspcorenn@gmail.com>
2021-03-30 19:34:35 +0700

    Push code

---
+++ Buoi06_ES6/ES6_02.js
@@ -0,0 +1,18 @@
+﻿//6. Destructing (object/array)
+let ngayThanhLap = [10, 3, 2003]
+let [ngay, thang, nam] = ngayThanhLap;
+console.log(ngay)
+console.log(thang)
+console.log(nam)
+let [, month,] = ngayThanhLap;
+console.log("Thang:", month)
+
+let hocVien = {
+    "ten": "Nhất Nghệ",
+    "tuoi": 18,
+    "conDiHoc": true
+}
+let { ten: tengi, tuoi, conDiHoc } = hocVien;
+console.log(tengi)
+console.log(tuoi)
+console.log(conDiHoc)
---
+++ d06component/src/App.js
@@ -0,0 +1,14 @@
+﻿import './App.css';
+//import * from './components/Demo';
+import { Demo, Hello } from './components/Demo';
+
+function App() {
+    return (
+        <div className="App">
+            <Demo name="Trần Tèo" />
+            <Hello name="Lý Tú" />
+        </div>
+    );
+}
+
+export default App;
