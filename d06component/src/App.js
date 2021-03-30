923e3c7f14bc4dab0db74a8c69a33441458578a5
Nhat Nghe <aspcorenn@gmail.com>
2021-03-30 20:25:46 +0700

    function component

--- d06component/src/App.js
+++ d06component/src/App.js
@@ -1,12 +1,49 @@
 ﻿import './App.css';
-//import * from './components/Demo';
 import { Demo, Hello } from './components/Demo';
+import { useState } from 'react';
 
 function App() {
+    const [hoTen, setHoTen] = useState('Nhất Nghệ');
+    const handleChange = (value) => {
+        console.log(value)
+        setHoTen(value)
+    }
+
+    //Khai báo state chứa danh sách các việc cần làm
+    const [items, setItems] = useState([
+        "Đi học ReactJS", "Xem phim", "Nghe nhạc"
+    ]);
+    const [value, setValue] = useState("");
+    const handleAddItem = () => {
+        //lấy giá trị của value thêm bào mảng Items
+        //let newValue = [...items, value]
+        let newValue = [value, ...items]
+        setItems(newValue);
+    }
+
     return (
         <div className="App">
-            <Demo name="Trần Tèo" />
-            <Hello name="Lý Tú" />
+            <h2>Danh sách công việc</h2>
+            {items.map(item => (
+                <div className="todo-item">{item}</div>
+            ))}
+            {items.map(item => {
+                let item_kq = item.toUpperCase();
+                return (
+                    <li>{item_kq}</li>
+                );
+            })}
+            <div>
+                Nhập công việc:
+                <input value={value} onChange={e => setValue(e.target.value)} />
+                <button onClick={handleAddItem}>Thêm</button>
+            </div>
+            <div>
+                Nhập tên:
+                <input onChange={e => handleChange(e.target.value)} />
+            </div>
+            <Demo name={hoTen} />
+            <Hello name={hoTen} />
         </div>
     );
 }
