0afd220f1df564f179fb418f7cf913db5b89253f
NN <aspcorenn@gmail.com>
2021-04-17 18:19:32 +0700

    Update GioHangReducer

--- d11sourcecode/src/reducers/GioHangReducer.js
+++ d11sourcecode/src/reducers/GioHangReducer.js
@@ -1,36 +1,32 @@
 ﻿// src/reducers/GioHangReducer.js
 //Xử lý cập nhật state mới dựa vào action gửi qua + state cũ
 import * as Types from '../constants/Types';
-
-let data = localStorage.getItem("GIOHANG");
-
-let initCartValue = data ? data : [];
+const initCartValue = [];
 
 let index = -1;
 export const Cart = (state = initCartValue, action) => {
     var { product, quantity } = action;
     switch (action.type) {
         case Types.ADD_PRODUCT_TO_CART:
-            console.log("AAAA")
-            index = findProductInCart(state, product);
+            index = findProductInCart(state, product.id);
             if (index > -1) {
                 state[index].quantity += quantity;
             } else {
                 state.push({ product, quantity });
             }            
-            return [...state];
+            return state;
         case Types.UPDATE_PRODUCT_IN_CART:
-            index = findProductInCart(state, product);
+            index = findProductInCart(state, product.id);
             if (index > -1) {
                 state[index].quantity = quantity;
             } 
-            return [...state];
+            return state;
         case Types.REMOVE_PRODUCT_OUT_CART:
-            index = findProductInCart(state, product);
+            index = findProductInCart(state, product.id);
             if (index > -1) {
                 state.slice(index, 1);
             }
-            return [...state];
+            return state;
         default: return state;
     }
 }
